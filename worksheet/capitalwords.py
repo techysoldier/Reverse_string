a5f3f5c08f755e14ea99559d40aae5ff81ec8f71
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-17 15:27:06 -0400

    updated cap

--- worksheet/capitalwords.py
+++ worksheet/capitalwords.py
@@ -1,3 +1,13 @@
+# Input lower case 
+# Output upper case
+# cap current index
+# if index -1 is space cap current  
+# append
+#return cap selection 
+#print cap format 
+
+
+
 def cap_letter(str):
     cap_words = str[0].upper()
     for i in range(1, len(str)):
