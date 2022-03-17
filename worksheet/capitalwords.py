0b977b12751058651ae93fb60e845cff5a80467f
Devin-Symone Johnson <devinsymone@gmail.com>
2022-03-17 15:23:20 -0400

    capital words update

--- worksheet/capitalwords.py
+++ worksheet/capitalwords.py
@@ -1,11 +1,10 @@
-#problem 2
+def cap_letter(str):
+    cap_words = str[0].upper()
+    for i in range(1, len(str)):
+        if (str[i-1] == " "):
+            cap_words += str[i].upper()
+        else:
+            cap_words += str[i]
+    return cap_words
 
-
-# from string import capwords
-
-# txt = input('Enter word')
-# def capitalizeFirstLetter(string):
-#     for txt in string.split("",""):
-#         capwords()
-# x = txt.capitalize()
-# print (x)
+print(cap_letter("hello world it's me"))
